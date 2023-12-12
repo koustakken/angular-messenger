c4e2e92694c35db586cd751811c1df1256e67c62
Artem Ivanov <ivanov@hostco.ru>
2023-12-12 18:35:01 +0500

    Декомпозиция на компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
+import { ConversationListComponent } from './components/conversation-list/conversation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    ChatMessageComponent,
+    ConversationListComponent
   ],
   imports: [
     BrowserModule,
